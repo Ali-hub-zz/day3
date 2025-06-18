2a99f43de29063fa9ffa4fc411b36d94f6c18172
Ali-hub-zz <mubashiralitz55@gmail.com>
2025-06-18 09:37:32 +0530

    added stringjs

---
+++ strings.js
@@ -0,0 +1,31 @@
+let str="this is a string "
+console.log(str.replaceAll(" ", "-"))
+
+// let watches="patek philippe"
+// let alternate = " audmare piguet "
+
+// let result=watches.concat(alternate);
+
+// console.log(result)
+
+let fruits="apple, banana, pomegranate"
+
+let change = fruits.split(",");
+
+console.log(change);
+
+let rep='he'
+
+let rep1=rep.repeat(3);
+
+console.log(rep1)
+
+
+let slice="fastrack"
+
+let sl=slice.slice(2,6);
+
+console.log(sl);
+
+
+
